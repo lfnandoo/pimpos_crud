9d75e668a8b2685df2c81b2ed679b35234bf906b
lfnandoo <devfernandoof@gmail.com>
2020-11-06 13:15:56 -0300

    feat: status bar added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
@@ -6,20 +7,27 @@ import * as Styles from './styles';
 
 const Header: React.FC = () => {
   return (
-    <Styles.Header
-      // eslint-disable-next-line react-native/no-inline-styles
-      style={{
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 12,
-        },
-        shadowOpacity: 1,
-        shadowRadius: 16.0,
-        elevation: 60,
-      }}>
-      <Styles.Image source={logoImg} />
-    </Styles.Header>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Styles.Header
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 12,
+          },
+          shadowOpacity: 1,
+          shadowRadius: 16.0,
+          elevation: 60,
+        }}>
+        <Styles.Image source={logoImg} />
+      </Styles.Header>
+    </>
   );
 };
 
