b459a47fec34bdb796b1c2eecb933c2621004225
lfnandoo <devfernandoof@gmail.com>
2020-11-07 04:48:15 -0300

    feat: cephalic perimeter mask

--- src/components/DecimalInput/index.tsx
+++ src/components/DecimalInput/index.tsx
@@ -59,34 +59,67 @@ const DecimalInput: React.FC<DecimalInputProps> = ({
             textParsed = maskNumber;
           }
           break;
-        case 'HEIGHT': {
+        case 'HEIGHT':
+          {
+            let maskNumber = textParsed,
+              integer = maskNumber.split(',')[0];
+
+            maskNumber = maskNumber.replace(/\D/, '');
+            maskNumber = maskNumber.replace(/^[0]+/, '');
+
+            if (maskNumber.length <= 4 || !integer) {
+              if (maskNumber.length === 1) {
+                maskNumber = '0,0' + maskNumber;
+              }
+              if (maskNumber.length === 2) {
+                maskNumber = '0,' + maskNumber;
+              }
+              if (maskNumber.length === 3) {
+                let firstNumber = maskNumber.charAt(0);
+                let secondNumber = maskNumber.charAt(1);
+                let thirdNumber = maskNumber.charAt(2);
+                maskNumber = `${firstNumber},${secondNumber}${thirdNumber}`;
+              }
+              if (maskNumber.charAt(1) !== ',' && maskNumber.charAt(0)) {
+                let firstNumber = maskNumber.charAt(0);
+                let thirdNumber = maskNumber.charAt(2);
+                let fourthNumber = maskNumber.charAt(3);
+                maskNumber = `${firstNumber},${thirdNumber}${fourthNumber}`;
+              }
+            } else {
+              maskNumber = maskNumber.replace(/^(\d{1,})(\d{4})$/, '$1,$2');
+            }
+            textParsed = maskNumber;
+          }
+          break;
+        case 'CEPHALIC_PERIMETER': {
           let maskNumber = textParsed,
-            integer = maskNumber.split(',')[0];
+            integer = maskNumber.split('.')[0];
 
           maskNumber = maskNumber.replace(/\D/, '');
           maskNumber = maskNumber.replace(/^[0]+/, '');
 
           if (maskNumber.length <= 4 || !integer) {
             if (maskNumber.length === 1) {
-              maskNumber = '0,0' + maskNumber;
+              maskNumber = '0.0' + maskNumber;
             }
             if (maskNumber.length === 2) {
-              maskNumber = '0,' + maskNumber;
+              maskNumber = '0.' + maskNumber;
             }
             if (maskNumber.length === 3) {
               let firstNumber = maskNumber.charAt(0);
               let secondNumber = maskNumber.charAt(1);
               let thirdNumber = maskNumber.charAt(2);
-              maskNumber = `${firstNumber},${secondNumber}${thirdNumber}`;
+              maskNumber = `${firstNumber}.${secondNumber}${thirdNumber}`;
             }
-            if (maskNumber.charAt(1) !== ',' && maskNumber.charAt(0)) {
+            if (maskNumber.charAt(1) !== '.' && maskNumber.charAt(0)) {
               let firstNumber = maskNumber.charAt(0);
               let thirdNumber = maskNumber.charAt(2);
               let fourthNumber = maskNumber.charAt(3);
-              maskNumber = `${firstNumber},${thirdNumber}${fourthNumber}`;
+              maskNumber = `${firstNumber}.${thirdNumber}${fourthNumber}`;
             }
           } else {
-            maskNumber = maskNumber.replace(/^(\d{1,})(\d{4})$/, '$1,$2');
+            maskNumber = maskNumber.replace(/^(\d{1,})(\d{4})$/, '$1.$2');
           }
           textParsed = maskNumber;
         }
